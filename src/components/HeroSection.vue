9c15d93ebfb72d87090a9f1aa78bde6c6bbb2dfc
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-17 17:32:40 +0200

    add: select input in HeroSection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,8 +1,44 @@
 <script>
+import axios from "axios";
+import { store } from '../store.js';
 export default {
     data() {
         return {
-
+            specializations: [],
+            doctors: null,
+            store,
+            selectSpecialization: ""
+        }
+    },
+    created() {
+        this.getDoctors();
+        this.getSpecialization();
+    },
+    methods: {
+        getSpecialization() {
+            axios.get(`${this.store.apiBaseURL}/api/specializations`).then((resp) => {
+                console.log(resp);
+                this.specializations = resp.data.results;
+            })
+        },
+        getDoctors() {
+            const params = {};
+            if (this.selectSpecialization !== "") {
+                params.specialization_id = this.selectSpecialization
+            }
+            axios.get(`${this.store.apiBaseURL}/api/doctors`, {
+                params
+            })
+                .then((resp) => {
+                    this.doctors = resp.data.results;
+                    console.log(this.doctors);
+                    this.$nextTick(() => {
+                        this.handleScroll();
+                    });
+                })
+                .catch(error => {
+                    console.error('Errore fetch dottori:', error);
+                });
         }
     }
 }
@@ -18,6 +54,14 @@ export default {
                 <p class="typewriter">Offriamo i migliori professionisti medici per la vostra salute.</p>
             </div>
         </section>
+
+        <div class="custom-select">
+            <select id="" aria-label="seleziona specializzazione" v-model="selectSpecialization" @change="getDoctors">
+                <option value="">Tutte le specializzazioni</option>
+                <option :value="specialization.id" v-for="specialization in specializations"> {{ specialization.title }}
+                </option>
+            </select>
+        </div>
     </div>
 
 </template>
@@ -34,7 +78,23 @@ export default {
     margin-bottom: 20px;
     padding: 20px;
     display: flex;
-    justify-content: end;
+    flex-direction: column;
+    align-items: end;
+
+    .custom-select {
+        margin-top: 20px;
+        width: 70%;
+        display: flex;
+        justify-content: center;
+
+        select {
+            border-radius: 10px;
+            padding: 10px;
+            border: 2px solid $glaucous;
+            color: $glaucous;
+            background-color: transparent;
+        }
+    }
 }
 
 /* Sezione di benvenuto */
@@ -59,30 +119,42 @@ export default {
         transform: translateY(-50px);
         opacity: 0;
     }
+
     to {
         transform: translateY(0);
-        opacity: 1; 
+        opacity: 1;
     }
 }
 
 /* Effetto di scrittura per il testo della sezione di benvenuto */
 .typewriter {
     overflow: hidden;
-    border-right: 5px solid rgba(146, 180, 244, 1); 
+    border-right: 5px solid rgba(146, 180, 244, 1);
     white-space: nowrap;
-    margin: 0 auto; 
-    letter-spacing: .15em; 
+    margin: 0 auto;
+    letter-spacing: .15em;
     animation: typing 3.5s steps(40, end), blink-caret .75s;
 }
 
 @keyframes typing {
-    from { width: 0 }
-    to { width: 100% }
+    from {
+        width: 0
+    }
+
+    to {
+        width: 100%
+    }
 }
 
 @keyframes blink-caret {
-    from, to { border-color: transparent }
-    50% { border-color: rgba(146, 180, 244, 1); }
-}
 
+    from,
+    to {
+        border-color: transparent
+    }
+
+    50% {
+        border-color: rgba(146, 180, 244, 1);
+    }
+}
 </style>
