7a52cd088b2e4e3cba95f0567d36405c733633ed
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-17 15:40:48 +0200

    add: nursing's photo

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,12 +1,19 @@
 <script>
+export default {
+    data() {
+        return {
 
+        }
+    }
+}
 </script>
 
 
 <template>
 
 <div>
-    
+    <h1>Benvenuti</h1>
+    <img src="" alt="">
 </div>
 
 </template>
