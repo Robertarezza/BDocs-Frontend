50497880a35999180168a8f33e894869742fee11
Robertarezza <roberta.rezza@gmail.com>
2024-07-18 11:40:31 +0200

    fix: color CardProfile

--- src/components/CardProfile.vue
+++ src/components/CardProfile.vue
@@ -18,7 +18,7 @@ export default {
           <p class="card-text ">Prenota una consulenza con il Dott. {{ doctor.user.name }} {{ doctor.user.surname }}</p>
           
           <div class="input">
-            <input class="input-submit" type="submit" value="Contattami" />
+            <input class="input-submit text-light" type="submit" value="Contattami" />
           </div>
         </div>
       </div>
@@ -29,7 +29,7 @@ export default {
             <h3 class="title">La tua opinione conta!</h3>
             <p class="card-text">Hai avuto un'esperienza con il Dr. {{ doctor.user.name }} {{ doctor.user.surname }}? Lascia una recensione e aiutaci a migliorare!</p>
             <div class="input">
-          <input class="input-submit" type="submit" value="LASCIACI UNA RECENSIONE" />
+          <input class="input-submit text-light" type="submit" value="Lascia una recensione"  />
         </div>
         </div>
       </div>
@@ -57,7 +57,7 @@ export default {
   border-radius: 10px;
   color: white;
   background-color: transparent;
-  background-image:linear-gradient(90deg, #0a369dff, #4472caff, #5e7ce2ff, #92b4f4ff, #cfdee7ff);
+  background-image:linear-gradient(45deg, #0a369dff, #4472caff, #5e7ce2ff, #92b4f4ff, #cfdee7ff);
   box-shadow: -3px 9px 34px 0 rgba(0, 0, 0, 0.25);
   width: 95%;
 }
@@ -71,6 +71,8 @@ export default {
   border-radius: 20px;
   background: #70707059;
   padding: 10px 30px;
+  width: 70%;
+  margin: 0 auto;
   //margin: 23px;
 
 //   .input-email {
@@ -88,6 +90,7 @@ export default {
   text-align: end;
   padding-left: 20px;
   padding-right: 5px;
+  
 }
 
 
@@ -97,7 +100,7 @@ export default {
 // }
 
 .cardsx {
-  background-image:linear-gradient(90deg, #0a369dff, #4472caff, #5e7ce2ff, #92b4f4ff, #cfdee7ff);
+  background-image:linear-gradient(45deg, #0a369dff, #4472caff, #5e7ce2ff, #92b4f4ff, #cfdee7ff);
   border-radius: 10px;
   color: white;
   width: 95%;
