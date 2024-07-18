dbdebc5fd81f8baa9bbad870862ff5773a51402e
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-07-18 11:07:27 +0200

    fix: carousel

--- src/components/PreFooter.vue
+++ src/components/PreFooter.vue
@@ -10,23 +10,62 @@ export default {
   <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner">
       <div class="ms_icon-color ms_bg1 text-center carousel-item active">
-        <div class="w-50">
-            <i class="fs-1 fa-solid fa-calendar-check"></i>
-            <h3 class="fw-bold py-3">Prenotazione di Appuntamenti Online</h3>
-            <p>
-              La piattaforma consente agli utenti di prenotare facilmente
-              appuntamenti medici online. Gli utenti possono selezionare il medico,
-              scegliere la data e l'orario disponibili, e ricevere conferme e
-              promemoria via email o SMS, rendendo il processo di prenotazione
-              semplice e comodo.
-            </p>
+        <div
+          class="w-50 ms-5 ms_height d-flex justify-content-center align-items-center flex-column"
+        >
+          <i class="fs-1 fa-solid fa-calendar-check"></i>
+          <h3 class="fw-bold py-3">Prenotazione di Appuntamenti Online</h3>
+          <p>
+            La piattaforma consente agli utenti di prenotare facilmente
+            appuntamenti medici online. Gli utenti possono selezionare il
+            medico, scegliere la data e l'orario disponibili, e ricevere
+            conferme e promemoria via email o SMS, rendendo il processo di
+            prenotazione semplice e comodo.
+          </p>
         </div>
       </div>
-      <div class="ms_bg2 carousel-item">
+      <div class="ms_icon-color ms_bg2 text-center carousel-item">
+        <div
+          class="w-50 m-auto ms_height d-flex justify-content-center align-items-center flex-column"
+        >
+        <i class="fs-1 fa-solid fa-user-doctor"></i>
+        <h3 class="fw-bold py-3">Consultazioni Mediche Online</h3>
+          <p>
+            Gli utenti possono accedere a consultazioni mediche online tramite
+            videoconferenza, telefono o chat. Questo servizio offre la comodità
+            di consultarsi con un medico da casa, ideale per chi ha difficoltà a
+            recarsi fisicamente in uno studio medico.
+          </p>
+        </div>
       </div>
-      <div class="ms_bg3 carousel-item">
+      <div class="ms_icon-color text-center ms_bg3 carousel-item">
+        <div class="d-flex justify-content-end ms_height align-items-center me-5">
+          <div class="w-50 d-flex flex-column align-items-center">
+            <i class="fs-1 fa-solid fa-comment-medical"></i>
+            <h3 class="fw-bold py-3">Accesso a Feedback e Recensioni</h3>
+            <p>
+              Gli utenti possono visualizzare feedback e recensioni sui medici
+              registrati sulla piattaforma. Questo aiuta gli utenti a fare scelte
+              più informate riguardo ai professionisti a cui rivolgersi, basandosi
+              sulle esperienze di altri pazienti.
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="ms_bg4 carousel-item">
+      <div class="ms_icon-color text-center ms_bg4 carousel-item">
+        <div
+          class="w-50 ms-5 ms_height d-flex justify-content-center align-items-center flex-column"
+        >
+        <i class="fs-1 fa-solid fa-bell"></i>
+        <h3 class="fw-bold py-3">Promemoria e Gestione delle Terapie</h3>
+          <p>
+            Gli utenti possono ricevere promemoria per le terapie e i farmaci
+            prescritti tramite notifiche via SMS o email. Questo servizio aiuta
+            gli utenti a seguire correttamente le indicazioni mediche e a non
+            dimenticare le somministrazioni, migliorando l'aderenza ai
+            trattamenti.
+          </p>
+        </div>
       </div>
     </div>
     <button
@@ -102,26 +141,46 @@ export default {
 <style scoped lang="scss">
 @use "../style/partials/variables" as *;
 .ms_bg1 {
-    background-image: url(../assets/img/nursing1.jpg);
-    background-size: cover;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url(../assets/img/nursing1.jpg);
+  background-size: cover;
 }
 
 .ms_bg2 {
-    background-image: url(../assets/img/nursing2.png);
-    background-size: cover;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url(../assets/img/nursing2.png);
+  background-size: cover;
 }
 
 .ms_bg3 {
-    background-image: url(../assets/img/nursing3.jpg);
-    background-size: cover;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url(../assets/img/nursing3.jpg);
+  background-size: cover;
 }
 
 .ms_bg4 {
-    background-image: url(../assets/img/nursing4.jpg);
-    background-size: cover;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2)
+    ),
+    url(../assets/img/nursing4.jpg);
+  background-size: cover;
+}
+
+.ms_height {
+  min-height: 500px;
 }
 
 .ms_icon-color {
-    color: $egyptian-blue;
+  color: $egyptian-blue;
 }
 </style>
