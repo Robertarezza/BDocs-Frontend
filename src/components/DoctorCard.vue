aef685a031a7945595807879585e0a7f51b224a6
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-07-22 16:54:32 +0200

    fix: star with rounded

--- src/components/DoctorCard.vue
+++ src/components/DoctorCard.vue
@@ -17,7 +17,7 @@ export default {
       if (this.doctor.ratings && this.doctor.ratings.length > 0) {
         const total = this.doctor.ratings.reduce((sum, rating) => sum + rating.rating, 0);
         
-        return total / this.doctor.ratings.length;
+        return Math.round(total / this.doctor.ratings.length);
       }
       return 0;
     }
