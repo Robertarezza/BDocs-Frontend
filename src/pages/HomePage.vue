6cdcf4f9562b688502fe0a5aabe5a54ca9d611c1
francesco-mauro <francesco.mauro180@gmail.com>
2024-07-23 16:19:44 +0200

    fix: Corrections to HomePage stretch

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -46,14 +46,15 @@ export default {
         params.specialization_id = this.selectSpecialization;
       }
       if (this.selectRating !== "") {
-        params.average_rating = this.selectRating;
-      }
+        params.average_rating = this.selectRating; // Assicurati che il parametro sia corretto
+    }
       axios
         .get(`${this.store.apiBaseURL}/api/doctors`, {
           params,
         })
         .then((resp) => {
           this.doctors = resp.data.results;
+          
           console.log("Dottori filtrati:", this.doctors);
           this.$nextTick(() => {
             this.handleScroll();
@@ -76,19 +77,19 @@ export default {
       });
     },
     handleScroll() {
-      const elements = document.querySelectorAll(".fade-in");
-      const windowHeight = window.innerHeight;
+    const elements = document.querySelectorAll(".fade-in");
+    const windowHeight = window.innerHeight;
 
-      elements.forEach((el) => {
-        const rect = el.getBoundingClientRect();
-        if (rect.top < windowHeight && rect.bottom > 200) {
-          el.classList.add("visible");
-        } else {
-          el.classList.remove("visible");
-        }
-      });
-    },
+    elements.forEach((el) => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < windowHeight && rect.bottom > 200) {
+        el.classList.add("visible");
+      } else {
+        el.classList.remove("visible");
+      }
+    });
   },
+},
 };
 </script>
 
@@ -101,6 +102,7 @@ export default {
     <div class="m-5 d-flex align-items-center justify-content-evenly query fade-in">
       <h6 class="m-0 media-h6">Scegli i nostri dottori in base alle loro prestazioni</h6>
       <div class="d-flex justify-content-center custom-select">
+        <!-- <SearchBar /> -->
         <select
           id=""
           aria-label="seleziona specializzazione"
@@ -120,9 +122,10 @@ export default {
     <div class="m-5 d-flex align-items-center justify-content-evenly query fade-in">
       <h6 class="m-0 media-h6">Scegli i nostri dottori in base ai loro voti</h6>
       <div class="d-flex justify-content-center custom-select">
+        <!-- <SearchBar /> -->
         <select
           id=""
-          aria-label="seleziona rating"
+          aria-label="seleziona specializzazione"
           v-model="selectRating"
           @change="getDoctors"
         >
@@ -266,7 +269,7 @@ html {
   opacity: 1;
 }
 
-// Tentativo di fade in delle cards
+// Tentativo di fae in delle cards
 
 //Select custom
 select {
@@ -280,36 +283,54 @@ h6 {
   color: rgb(10, 54, 157);
 }
 
-/* Media Queries per aggiustamenti extra */
+// responsive media
+
+.media-p {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  white-space: normal;
+}
+
+
+
+
+
 @media (min-width: 768px) {
   .col {
-    padding: 1.5rem; /* Regola il padding per schermi grandi */
+    padding: 1.5rem; 
   }
 }
 
+/* Media Queries per aggiustamenti extra */
 @media (max-width: 576px) {
   .col {
-    padding: 0.5rem; /* Regola il padding per schermi piccoli */
+    padding: 0.5rem; 
   }
   .media-h1 {
-    font-size: 1.7rem; /* Riduci la dimensione del font per schermi piccoli */
+    font-size: 1.7rem;
   }
   .media-p {
-    font-size: 0.9rem; /* Riduci la dimensione del font per schermi piccoli */
+    font-size: .9rem;
   }
   .media-h6 {
-    font-size: 0.9rem; /* Riduci la dimensione del font per schermi piccoli */
+    font-size: 0.9rem;
     text-align: center;
   }
+
 }
 
+
+
 @media screen and (max-width: 450px) {
-  .query {
-    flex-direction: column; /* Modifica la direzione del flex per schermi molto piccoli */
+
     
-    .media-h6 {
-      padding-bottom: 8px; /* Aggiungi padding per separare gli elementi */
+.query {
+    flex-direction: column;
+    
+    .media-h6{
+        padding-bottom: 8px;
+        
     }
-  }
+}
 }
 </style>
